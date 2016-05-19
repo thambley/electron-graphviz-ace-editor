4b22ca82591020b2de10007f4f5978a9d529704b
Todd Hambley <trhambley@yahoo.com>
2016-05-18 22:05:07 -0400

    re-add environment checker

---
+++ scripts/env.js
@@ -0,0 +1,14 @@
+// https://github.com/diversen/electron-markdown-editor
+
+let app = {
+    env: '',
+    agent: ''
+};
+
+if (window && window.process && window.process.type) {
+    app.env = 'electron';
+} else {
+    app.env = 'browser';
+}
+
+
